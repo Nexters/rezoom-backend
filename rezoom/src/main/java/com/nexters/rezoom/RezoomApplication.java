6d0f1ae8e40d70811854c28aaae8e04dc8cf276a
JeongHo <wwwkang8@gmail.com>
2018-07-17 20:04:04 +0900

    강정호 커밋 jhkang branch

---
+++ rezoom/src/main/java/com/nexters/rezoom/RezoomApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.rezoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezoomApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RezoomApplication.class, args);
+	}
+	
+}
---
+++ rezoom/src/main/java/com/nexters/rezoom/domain/Example.java
@@ -0,0 +1,9 @@
+package com.nexters.rezoom.domain;
+
+public class Example {
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+}
