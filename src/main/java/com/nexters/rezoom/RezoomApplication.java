a05ec67219057d3130abc4574a40be2b47d6bf72
jinjaeeon <momentjin@gmail.com>
2018-07-18 16:10:08 +0900

    spring security + oauth2 social login 구현

--- src/main/java/com/nexters/rezoom/RezoomApplication.java
+++ src/main/java/com/nexters/rezoom/RezoomApplication.java
@@ -2,12 +2,13 @@ package com.nexters.rezoom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
+@EnableOAuth2Client
 public class RezoomApplication {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(RezoomApplication.class, args);
 	}
-	
 }
--- src/main/java/com/nexters/rezoom/domain/Example.java
+++
@@ -1,9 +0,0 @@
-package com.nexters.rezoom.domain;
-
-public class Example {
-	private String value;
-
-	public String getValue() {
-		return value;
-	}
-}
