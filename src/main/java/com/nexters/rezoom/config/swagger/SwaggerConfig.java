7170154b7a240c407ccab622786fb7fd86bc2bb8
jinjaeeon <momentjin@gmail.com>
2018-08-02 04:14:51 +0900

    swagger 기본 설정

---
+++ src/main/java/com/nexters/rezoom/config/swagger/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.nexters.rezoom.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by JaeeonJin on 2018-07-31.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    /**
+     * TODO : 현재 기본설정만 되어있는 상태. 수정 필요
+     */
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
