617e44ebe7729274ae2590b39e197fe9d583cba5
YejiLee <32984517+yjll1019@users.noreply.github.com>
2018-08-09 13:26:51 +0900

    예지 : 입력한 모든 해시태그 조회
    예지 : 입력한 모든 해시태그 조회

--- src/main/java/com/nexters/rezoom/repository/QuestionRepository.java
+++ src/main/java/com/nexters/rezoom/repository/QuestionRepository.java
@@ -1,11 +1,12 @@
 package com.nexters.rezoom.repository;
 
-import com.nexters.rezoom.dto.QuestionDTO;
-import com.nexters.rezoom.dto.QuestionListResponseDTO;
+import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
+import com.nexters.rezoom.domain.HashTagList;
+import com.nexters.rezoom.dto.QuestionDTO;
+import com.nexters.rezoom.dto.QuestionListResponseDTO;
 
 public interface QuestionRepository {
 
@@ -17,5 +18,7 @@ public interface QuestionRepository {
 
     // 이력서 내 단일 문항 상세보기
     QuestionDTO getQuestion(@Param("username") String username, @Param("resumeId") int resumeId, @Param("questionId") int questionId);
+    
+    List<HashTagList> getAllHashTag(String username);
 
 }
