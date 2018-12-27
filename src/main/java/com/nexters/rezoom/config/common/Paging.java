aacbdec84494134a92742050cd1f430d730a149f
momentjin <momentjin@gmail.com>
2018-12-27 21:56:59 +0900

    refactor: paging 클래스 추가 및 간단한 테스트 코드 추가
    - 페이지번호를 넣었을 때, beginRow와 pagePerRow값을 설정하는 클래스
    : JPA Repository에서 사용 (페이징처리된 List 가져오려면 beginRow와 페이지당 row갯수를 알아야함)
    
    Signed-off-by: momentjin <momentjin@gmail.com>

--- src/main/java/com/nexters/rezoom/config/common/Paging.java
+++ src/main/java/com/nexters/rezoom/config/common/Paging.java
@@ -6,34 +6,29 @@ import lombok.Getter;
 @Getter
 public class Paging {
 
-    private static int DEFAULT_PAGE_NUMBER = 1;
+    private static int MIN_PAGE_NUMBER = 1;
     private static int MAX_NUMBER_PER_PAGE = 10;
 
     private int pageNo;
     private int beginRow;
-    private int numberPerPage;
+    private int numberPerPage = MAX_NUMBER_PER_PAGE;
 
     public Paging(int pageNo) {
         this.setPageNo(pageNo);
-        this.setNumberPerPage();
         this.setBeginRow();
     }
 
     private void setPageNo(int pageNo) {
-        if (pageNo < 0) {
-            this.pageNo = DEFAULT_PAGE_NUMBER;
+        if (pageNo <= 0) {
+            this.pageNo = MIN_PAGE_NUMBER;
             return;
         }
 
         this.pageNo = pageNo;
     }
 
-    private void setNumberPerPage() {
-        this.numberPerPage = MAX_NUMBER_PER_PAGE;
-    }
-
     private void setBeginRow() {
-        this.beginRow = this.numberPerPage * (this.pageNo - 1);
+        this.beginRow = MAX_NUMBER_PER_PAGE * (this.pageNo - 1);
     }
 
 
